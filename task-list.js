0005e3d010e921c9605962d9dda5c8a2792e9b45
jafarsalimi <jfrslymy454@gmail.com>
2025-04-08 23:36:55 +0330

    4 commit

--- task-list.js
+++ task-list.js
@@ -7,8 +7,9 @@ const start_date_update=document.getElementById("start-date-form-update");
 const end_date_update=document.getElementById("end-date-form-update");
 const level_update=document.getElementById("level-form-update");
 const edite_box=document.querySelector('.edite-box');
-async function get_data_api(){
 
+
+async function get_data_api(){
     let res = await fetch('https://67e583ab18194932a5865f58.mockapi.io/api/task', {
         method: 'GET',
         headers: {'content-type':'application/json'},
@@ -16,8 +17,8 @@ async function get_data_api(){
     if (res.ok){
         return res.json();
     }
-        
 }
+
 async function get_task_data_api(id){
     let res = await fetch(`https://67e583ab18194932a5865f58.mockapi.io/api/task/${id}`, {
         method: 'GET',
@@ -50,8 +51,6 @@ async function fetchData() {
         new_div_btn.appendChild(delete_btn);
         new_li.appendChild(new_div_btn);
         task_list.appendChild(new_li);
-
-
     });
 }
 async function delete_task(id){
@@ -81,14 +80,44 @@ async function delete_box_manager(id) {
 async function edite_box_manager(id){
     let data= await get_task_data_api(id);
     edite_box.style.display='block';
+    document.getElementById('overlay').style.display = 'block';
+
     text_update.value=data.text;
     start_date_update.value=data.start_date;
     end_date_update.value=data.end_date;
     level_update.value=data.level;
+    document.getElementById("update-form").addEventListener("submit",()=>update_task(data.id));
+}
+function cancel_update_box(){
+    edite_box.style.display='none';
+    document.getElementById('overlay').style.display = 'none';
 
 }
+async function apply_update(id,text,start_date,end_date,level){
+    let res=await fetch(`https://67e583ab18194932a5865f58.mockapi.io/api/task/${id}`, {
+        method: 'PUT', // or PATCH
+        headers: {'content-type':'application/json'},
+        body: JSON.stringify(
+            {text:text,
+            start_date:start_date,
+            end_date:end_date,
+            level:level }
+            )
+      });
+      if (res.ok){
+        fetchData();
+      }
+}
+async function update_task(id){
+    let text_update_value=text_update.value;
+    let start_date_value=start_date_update.value;
+    let end_date_value=end_date_update.value;
+    let level_value=level_update.value;
+    await apply_update(id,text_update_value,start_date_value,end_date_value,level_value);
+    edite_box.style.display='none';
+    document.getElementById('overlay').style.display = 'none';
 
-
+}
 fetchData();
 document.querySelectorAll(".task-lists-btn").forEach(button=>
     button.addEventListener("click",function(){
